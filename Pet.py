62cee35e573d1c4acd74a3abc66590bb78881f2b
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-07-03 17:18:34 +0800

    Defining the objects for the Pet Class

--- Pet.py
+++ Pet.py
@@ -1,2 +1,25 @@
 # Creating the class Pet
 class Pet:
+    # Defining the objects for the Pet Class
+    def __init__(self):
+        self.__name = ""
+        self.__animal_type = ""
+        self.__age = 0
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
+
+    def get_name(self):
+        return self.__name
+
+    def get_animal_type(self):
+        return self.__animal_type
+
+    def get_age(self):
+        return self.__age
